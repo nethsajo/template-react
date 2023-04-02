5804bfa88370d86a3defce6ae2d2318ff67bec7a
Jan Kenneth Sajo <nethsajo98@gmail.com>
2023-04-02 13:20:05 +0800

    chore: update vite configs (specified base, server port, and folder aliases)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import { config } from 'dotenv';
+import react from '@vitejs/plugin-react';
+
+config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  server: {
+    port: Number(process.env.PORT),
+  },
+  preview: {
+    port: 8080,
+  },
+  define: {
+    'process.env.PORT': `${process.env.PORT}`,
+    'process.env.TEMPLATE': `"${process.env.TEMPLATE}"`,
+  },
+  resolve: {
+    alias: [
+      { find: '@/', replacement: '/src' },
+      { find: '@/Assets', replacement: '/src/assets' },
+      { find: '@/Modules', replacement: '/src/modules' },
+      { find: '@/Pages', replacement: '/src/pages' },
+      { find: '@/Shared', replacement: '/src/shared' },
+    ],
+  },
 });
