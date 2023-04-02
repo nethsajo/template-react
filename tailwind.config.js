53270625a42ba1cd38550ed364c69d834c5aa4fb
Jan Kenneth Sajo <nethsajo98@gmail.com>
2023-04-02 13:38:48 +0800

    chore: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./src/*.{js,jsx}",
+    "./src/modules/**/*.{js,jsx}",
+    "./src/pages/**/*.{js,jsx}",
+    "./src/shared/components/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
