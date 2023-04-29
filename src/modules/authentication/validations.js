6aad2d4b6d20dd1c15d02736b16a50af8be32c7b
Jan Kenneth Sajo <nethsajo98@gmail.com>
2023-04-29 13:40:43 +0800

    chore: update todo and login schema validation

--- src/modules/authentication/validations.js
+++ src/modules/authentication/validations.js
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-  email: z.string().email('Email is required'),
+  email: z.string().email('Email is required').toLowerCase(),
   password: z.string().min(8, 'Password must be at least 8 characters long.'),
 });
--- src/modules/todos/components/todos.jsx
+++ src/modules/todos/components/todos.jsx
@@ -1,65 +1,27 @@
-import { todoSchema } from '../validations';
-
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
 import { RiTodoLine } from 'react-icons/ri';
 
 export const Todos = () => {
-  const { handleSubmit, register, formState } = useForm({
-    resolver: zodResolver(todoSchema),
-    defaultValues: {
-      title: '',
-      description: '',
-    },
-  });
-
-  const handleTodos = data => console.log(data);
-
   return (
     <div className="flex flex-col items-center justify-center gap-11 px-6 py-9 sm:px-12">
       <RiTodoLine className="h-16 w-16 text-cyan-500 sm:h-24 sm:w-24" />
       <h1 className="text-center text-4xl font-extrabold text-slate-500 sm:text-6xl">TodoMate</h1>
       <div className="mx-auto w-full max-w-3xl rounded-md border-[#ebeef2] bg-white px-6 py-8 shadow-[0_1px_3px_rgba(96,108,128,0.05)]">
-        <form onSubmit={handleSubmit(handleTodos)}>
-          <div className="mb-4">
-            <label htmlFor="title" className="mb-2 flex flex-col">
-              <span className="mb-2 font-bold text-slate-500">Title</span>
-              <input
-                type="text"
-                id="title"
-                className="w-full rounded-sm bg-gray-100 px-4 py-2 font-medium text-slate-500 outline-none"
-                {...register('title')}
-              />
-            </label>
-            {formState.errors?.title?.message && (
-              <p className="text-sm font-semibold text-red-500">{formState.errors.title.message}</p>
-            )}
+        <div className="space-y-4">
+          <div className="flex items-center rounded-sm bg-gray-100 p-4">
+            <span className="flex-1">Test</span>
+            <div className="ml-auto flex items-center space-x-4">
+              <span>Edit</span>
+              <span>Delete</span>
+            </div>
           </div>
-          <div className="mb-4">
-            <label htmlFor="description" className="mb-2 flex flex-col">
-              <span className="mb-2 font-bold text-slate-500">Description</span>
-              <textarea
-                rows={3}
-                id="description"
-                className="w-full resize-none rounded-sm bg-gray-100 px-4 py-2 font-medium text-slate-500 outline-none"
-                {...register('description')}
-              />
-            </label>
-            {formState.errors?.description?.message && (
-              <p className="text-sm font-semibold text-red-500">
-                {formState.errors.description.message}
-              </p>
-            )}
+          <div className="flex items-center rounded-sm bg-gray-100 p-4">
+            <span className="flex-1">Test</span>
+            <div className="ml-auto flex items-center space-x-4">
+              <span>Edit</span>
+              <span>Delete</span>
+            </div>
           </div>
-          <div className="mt-8 flex sm:justify-end">
-            <button
-              type="submit"
-              className="w-full cursor-pointer rounded-sm bg-cyan-500 px-6 py-2 font-bold text-white sm:w-auto"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   );
